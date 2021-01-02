562d57640d87d990c27f18f87843fa2265c8eef0
Caroline Manson <carolinemanson15@gmail.com>
2021-01-02 11:52:12 -0500

    started building out employeelistitem

--- src/components/EmployeeListItem/index.js
+++ src/components/EmployeeListItem/index.js
@@ -1,14 +1,34 @@
-import React from "react";
-import ListGroup from 'react-bootstrap/ListGroup';
+import React, { Component } from "react";
+import SearchBar from "./SearchBar";
+import API from "..utils/API"
 
-function EmployeeListItem() {
-  return (
-    <ListGroup>
-        <ListGroup.Item>
-            Hi
-        </ListGroup.Item>
-    </ListGroup>
-  );
+// class component to render API results
+class EmployeeListItem extends Component {
+  state = {
+    search: "",
+    results: []
+  }
+
+// using this lifecycle method now so that the first render completes before this is executed
+componentDidMount() {
+  this.searchRandomUser("")
+}
+
+// custom methods to handle input change and form submit
+handleInputChange = event => {
+  const name = event.target.name;
+  const value = event.target.value;
+  this.setState({
+    [name]: value
+  });
+};
+
+handleFormSubmit = event => {
+  event.preventDefault();
+  this.searchRandomUser(this.state.search);
+};
+
+
 }
 
 
