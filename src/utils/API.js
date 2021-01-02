0a4500fabc9fbc55f722f5214a69bf209f3b4373
Caroline Manson <carolinemanson15@gmail.com>
2021-01-02 00:01:05 -0500

    fixed utils folder

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,15 +1,9 @@
 import axios from "axios";
 
-// Export an object containing methods we'll use for accessing the Dog.Ceo API
+const BASEURL = "https://randomuser.me/api/?";
 
 export default {
-  getRandomDog: function() {
-    return axios.get("https://dog.ceo/api/breeds/image/random");
-  },
-  getDogsOfBreed: function(breed) {
-    return axios.get("https://dog.ceo/api/breed/" + breed + "/images");
-  },
-  getBaseBreedsList: function() {
-    return axios.get("https://dog.ceo/api/breeds/list");
+  search: function(query) {
+    return axios.get(BASEURL + query);
   }
 };
