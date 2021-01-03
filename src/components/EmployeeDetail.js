538c0f2942608bc1d4df69f551080e8f53353fcb
Caroline Manson <carolinemanson15@gmail.com>
2021-01-02 21:34:35 -0500

    trying to pass props thruemployeedetail

--- src/components/EmployeeDetail.js
+++ src/components/EmployeeDetail.js
@@ -1,17 +1,13 @@
 import React from "react";
 
-function EmployeeDetail() {
+function EmployeeDetail(props) {
   return (
   <div className="text-center">
-    {/* {props.results.map(result => (
-      <li className="list-group-item" key={result.id}>
-        <img alt="Employee" className="img-fluid" src={props.result.picture.thumbnail} style={{ margin: "0 auto" }} />
-        <h3>{props.result.name.first} {props.result.name.last}</h3>
-        <h3>Call Office {props.results.phone}</h3>
-        <h3>Call Mobile {props.results.cell}</h3>
-        <h3>Email {props.results.email}</h3>
-    </li>
-    ))} */}
+      <img alt={props.name} className="img-fluid" src={props.picture.thumbnail} style={{ margin: "0 auto" }} />
+        <h3>{props.name.first} {props.name.last}</h3>
+        <h3>Call Office {props.phone}</h3>
+        <h3>Call Mobile {props.cell}</h3>
+        <h3>Email {props.email}</h3>
     </div>  
   );
 }
